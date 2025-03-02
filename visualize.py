34b5c1da42046b87800520338ff8fb51dc3afa50
NAKASONE Hiroshi <43738441+hiroshinakasone@users.noreply.github.com>
2025-03-02 18:03:46 +0900

    fix image width

--- visualize.py
+++ visualize.py
@@ -7,8 +7,8 @@ def main():
     input_file = sys.argv[1]
     csv = pd.read_csv(input_file)
     csv.sort_values(by=["distance", "file1", "file2"], ascending=True, inplace=True)
-    csv["image1"] = csv["file1"].apply(lambda x: f"<img src='{x}' width='100%'>")
-    csv["image2"] = csv["file2"].apply(lambda x: f"<img src='{x}' width='100%'>")
+    csv["image1"] = csv["file1"].apply(lambda x: f"<img src='{x}' width='400px'>")
+    csv["image2"] = csv["file2"].apply(lambda x: f"<img src='{x}' width='400px'>")
     with open(f"{input_file}.html", "w") as f:
         f.write(csv.to_html(index=False, escape=False))
 
